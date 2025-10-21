15223f972f891b8d5603df8d2f4c958fe500cad9
lZh1r <electroxhackerpig09@gmail.com>
2025-10-21 15:58:25 +0300

    style(sidebar): made sidebar trigger absolute

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,14 +4,17 @@ import {SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar.tsx";
 import AppSidebar from "@/components/layout/AppSidebar.tsx";
 import {Outlet} from "react-router";
 import ProfileMenu from "@/components/layout/ProfileMenu.tsx";
+import {useState} from "react";
 
 export default function Layout() {
 
+    const [open, setOpen] = useState(localStorage.getItem("sidebar_state") === "true");
+
     return (
         <>
-            <SidebarProvider defaultOpen={localStorage.getItem("sidebar_state") === "true"}>
+            <SidebarProvider open={open} onOpenChange={setOpen} defaultOpen={localStorage.getItem("sidebar_state") === "true"}>
                 <AppSidebar/>
-                <Button asChild className={"z-10 m-3"} variant={"outline"} size={"icon-lg"}>
+                <Button asChild className={`${open && "translate-x-[13rem]"} left-[3rem] z-10 m-3 absolute`} variant={"outline"} size={"icon-lg"}>
                     <SidebarTrigger/>
                 </Button>
                 <div className={"absolute right-0 p-3 flex space-x-3"}>
