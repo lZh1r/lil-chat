07a19dcd99d8a6978fcd49ed5446efe3ea13584e
lZh1r <electroxhackerpig09@gmail.com>
2025-10-22 16:08:45 +0300

    feat(chat): chat deletion

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -5,20 +5,37 @@ import {
     SidebarGroupContent,
     SidebarHeader,
     SidebarMenu,
+    SidebarMenuAction,
     SidebarMenuButton,
     SidebarMenuItem,
     SidebarMenuSub,
     SidebarMenuSubButton,
+    SidebarMenuSubItem,
     useSidebar,
 } from "@/components/ui/sidebar.tsx";
-import {ChevronRight} from "lucide-react";
+import {ChevronRight, Trash} from "lucide-react";
 import {Collapsible, CollapsibleContent, CollapsibleTrigger} from "@/components/ui/collapsible.tsx";
 import AppSidebarTopGroup from "@/components/layout/AppSidebarTopGroup.tsx";
 import {useEffect} from "react";
+import {useLiveQuery} from "dexie-react-hooks";
+import {db} from "@/db.ts";
+import {Link, useNavigate} from "react-router";
 
 export default function AppSidebar() {
 
+    const navigate = useNavigate();
     const sidebarState = useSidebar();
+    const chats = useLiveQuery(() => {
+        return db.chats.toArray();
+    }, [db.chats]);
+
+    async function deleteChat(chatId: string) {
+        await db.chats.delete(chatId);
+        await db.messages.where("chatId").equals(chatId).delete();
+        if (window.location.href.includes(chatId)) {
+            navigate("/");
+        }
+    }
 
     useEffect(() => {
         localStorage.setItem("sidebar_state", String(sidebarState.open));
@@ -49,9 +66,24 @@ export default function AppSidebar() {
                                     </CollapsibleTrigger>
                                     <CollapsibleContent>
                                         <SidebarMenuSub>
-                                            <SidebarMenuSubButton><span>Chat #1</span></SidebarMenuSubButton>
-                                            <SidebarMenuSubButton><span>Chat #2</span></SidebarMenuSubButton>
-                                            <SidebarMenuSubButton><span>Chat #3</span></SidebarMenuSubButton>
+                                            {
+                                                chats?.map(chat =>
+                                                    <SidebarMenuSubItem className={"flex space-x-2"} key={chat.id}>
+                                                        <SidebarMenuSubButton asChild>
+                                                            <Link to={`/chat/${chat.id}`}>
+                                                                <span className={"line-clamp-1 wrap-anywhere"}>
+                                                                    {chat.name}
+                                                                </span>
+                                                            </Link>
+                                                        </SidebarMenuSubButton>
+                                                        <SidebarMenuAction
+                                                            className={"p-1 w-fit h-fit static place-self-center"}
+                                                            onClick={() => deleteChat(chat.id)}>
+                                                            <Trash/>
+                                                        </SidebarMenuAction>
+                                                    </SidebarMenuSubItem>
+                                                )
+                                            }
                                         </SidebarMenuSub>
                                     </CollapsibleContent>
                                 </SidebarMenu>
