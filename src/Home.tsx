0e50bae3e8d01aad94e1697b4b0628e3f95b8d8f
lZh1r <electroxhackerpig09@gmail.com>
2025-10-22 11:28:31 +0300

    feat(chat): writing a message on main screen creates a new chat

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,40 @@
 import ChatInput from "@/components/ChatInput.tsx";
+import {db} from "@/db.ts";
+import {useNavigate} from "react-router";
 
 function Home() {
 
+    const navigate = useNavigate();
+
+    async function sendMessage(message: string) {
+        try {
+            let chatId = crypto.randomUUID();
+
+            while (await db.chats.get({id: chatId})) chatId = crypto.randomUUID();
+
+            await db.transaction("rw", [db.chats, db.messages], async () => {
+                await db.chats.add({
+                    id: chatId,
+                    name: `Chat ${chatId}`
+                });
+                await db.messages.add({
+                    chatId,
+                    role: "user",
+                    content: message
+                });
+            });
+
+            navigate(`/chat/${chatId}`);
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     return(
         <div className={"h-screen w-full flex flex-col justify-center space-y-5"}>
-            <h1 className={"text-center text-2xl"}>Welcome back</h1>
-            <ChatInput sendMessage={(msg) => console.log(msg)} className={"place-self-center w-1/2"}/>
+            <h1 className={"text-center text-3xl"}>Welcome back</h1>
+            <ChatInput sendMessage={sendMessage} className={"place-self-center w-1/2"}/>
         </div>
     );
 }
