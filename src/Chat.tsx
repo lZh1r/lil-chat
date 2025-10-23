6b60eae9b27fad56e0e7e7be0cac962731deb5d2
lZh1r <electroxhackerpig09@gmail.com>
2025-10-23 21:57:17 +0300

    feat: chatting with models!

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -3,9 +3,13 @@ import ChatInput from "@/components/ChatInput.tsx";
 import {db} from "@/lib/db.ts";
 import {useLiveQuery} from "dexie-react-hooks";
 import MessageBox from "@/components/MessageBox.tsx";
+import type {ModelMessage, ModelRequest, ModelResponse} from "@/lib/types.ts";
+import {useEffect, useRef, useState} from "react";
 
 export default function Chat() {
     const params = useParams<{chatId: string}>();
+    const [pendingMessage, setPendingMessage] = useState<null | ModelMessage>(null);
+    const scrollAreaRef = useRef<null | HTMLDivElement>(null);
 
     const messages = useLiveQuery(async () => {
         return db.messages.where("chatId").equals(params.chatId!).toArray();
@@ -18,19 +22,76 @@ export default function Chat() {
                 role: "user",
                 content: message
             });
+            scrollAreaRef.current!.scrollTop = scrollAreaRef.current!.scrollHeight;
+            await sendRequest(message);
         } catch (e) {
             console.log(e);
         }
     }
 
+    async function sendRequest(message: string) {
+        const context = messages! as ModelMessage[];
+        context.push({
+            role: "user",
+            content: message,
+        });
+        const requestBody: ModelRequest = {
+            model: "phi4-mini:latest",
+            messages: context
+        };
+        const response = await fetch("http://localhost:11434/api/chat", {
+            method: "POST",
+            body: JSON.stringify(requestBody)
+        });
+        const streamReader = response.body?.getReader();
+        let intermediateResult;
+        let result = "";
+        do {
+            const chunk = await streamReader?.read();
+            const decoded = new TextDecoder().decode(chunk?.value);
+            const decodedArray = decoded.split("\n").filter(s => s.length !== 0);
+            for (const i of decodedArray) {
+                intermediateResult = JSON.parse(i) as ModelResponse;
+                result += intermediateResult.message.content;
+                setPendingMessage({
+                    role: "assistant",
+                    content: result
+                });
+                scrollAreaRef.current!.scrollTop = scrollAreaRef.current!.scrollHeight;
+            }
+        } while (!intermediateResult!.done);
+        db.messages.add({
+            chatId: params.chatId!,
+            content: result,
+            role: "assistant"
+        });
+        setPendingMessage(null);
+    }
+
+    useEffect(() => {
+        scrollAreaRef.current!.scrollTop = scrollAreaRef.current!.scrollHeight;
+    }, [messages]);
+
     return (
-        <div className={"h-screen w-full flex flex-col justify-between"}>
-            <div className={"w-1/2 p-4 h-full place-self-center space-y-2"}>
+        <div className={"h-screen w-full flex flex-col justify-between p-2"}>
+            <div
+                ref={scrollAreaRef}
+                className={`
+                    w-2/3 max-w-2/3 p-4 h-[85vh] place-self-center space-y-2 wrap-anywhere overflow-y-scroll
+                    [&::-webkit-scrollbar-thumb]:hover:bg-white [&::-webkit-scrollbar-thumb]:transition-all
+                    [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:rounded-full 
+                    [&::-webkit-scrollbar-track]:bg-stone-900 [&::-webkit-scrollbar-thumb]:rounded-full 
+                    [&::-webkit-scrollbar-thumb]:bg-stone-800
+                `}
+            >
                 {
                     messages?.map(msg => <MessageBox key={msg.id} message={msg}/>)
                 }
+                {
+                    pendingMessage && <MessageBox message={pendingMessage}/>
+                }
             </div>
-            <div className={"p-4 w-1/2 place-self-center"}>
+            <div className={"p-2 w-1/2 place-self-center fixed bottom-0"}>
                 <ChatInput sendMessage={sendMessage} className={"place-self-center w-full"}/>
             </div>
         </div>
