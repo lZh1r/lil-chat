ea7c6145164e6fac0441bacbf656e7ce7acc0725
lZh1r <electroxhackerpig09@gmail.com>
2025-10-27 10:34:26 +0300

    feat(chat): error message

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -16,12 +16,14 @@ export default function Chat() {
     const [pendingMessage, setPendingMessage] = useState<null | ModelMessage>(null);
     const [inProgress, setInProgress] = useState(false);
     const selectedModel = useAtomValue(currentModel);
+    const [error, setError] = useState<null | string>(null);
 
     const messages = useLiveQuery(async () => {
         return db.messages.where("chatId").equals(chatId!).toArray();
     }, [chatId, db.messages]);
 
     const sendRequest = useCallback(async (message: string) => {
+        setError(null);
         setInProgress(true);
         const model = selectedModel === "auto" ? defaultModel : selectedModel;
         const context = (messages ?? []) as ModelMessage[];
@@ -33,46 +35,53 @@ export default function Chat() {
             model,
             messages: context
         };
-        const response = await fetch("http://localhost:11434/api/chat", {
-            method: "POST",
-            body: JSON.stringify(requestBody)
-        });
-        const streamReader = response.body?.getReader();
-        let intermediateResult: ModelResponse | undefined = undefined;
-        let result = "";
-        do {
-            const chunk = await streamReader?.read();
-            const decoded = new TextDecoder().decode(chunk?.value);
-            const decodedArray = decoded.split("\n").filter(s => s.length !== 0);
-            for (const i of decodedArray) {
-                intermediateResult = JSON.parse(i) as ModelResponse;
-                result += intermediateResult.message.content;
-                setPendingMessage({
-                    role: "assistant",
-                    content: result
-                });
-                window.scrollBy({left: 0, top: window.innerHeight * 1000});
-            }
-        } while (!intermediateResult!.done);
 
-        db.transaction("rw", [db.responseDetails, db.messages], async tx => {
-            const messageId = await tx.messages.add({
-                chatId: chatId!,
-                content: result,
-                role: "assistant"
+        try {
+            const response = await fetch("http://localhost:11434/api/chat", {
+                method: "POST",
+                body: JSON.stringify(requestBody)
             });
-            tx.responseDetails.add({
-                messageId,
-                chatId: chatId!,
-                model,
-                total_duration: intermediateResult?.total_duration,
-                load_duration: intermediateResult?.load_duration,
-                prompt_eval_count: intermediateResult?.prompt_eval_count,
-                prompt_eval_duration: intermediateResult?.prompt_eval_duration,
-                eval_count: intermediateResult?.eval_count,
-                eval_duration: intermediateResult?.eval_duration
+            const streamReader = response.body?.getReader();
+            let intermediateResult: ModelResponse | undefined = undefined;
+            let result = "";
+            do {
+                const chunk = await streamReader?.read();
+                const decoded = new TextDecoder().decode(chunk?.value);
+                const decodedArray = decoded.split("\n").filter(s => s.length !== 0);
+                for (const i of decodedArray) {
+                    intermediateResult = JSON.parse(i) as ModelResponse;
+                    result += intermediateResult.message.content;
+                    setPendingMessage({
+                        role: "assistant",
+                        content: result
+                    });
+                    window.scrollBy({left: 0, top: window.innerHeight * 1000});
+                }
+            } while (!intermediateResult!.done);
+
+            db.transaction("rw", [db.responseDetails, db.messages], async tx => {
+                const messageId = await tx.messages.add({
+                    chatId: chatId!,
+                    content: result,
+                    role: "assistant"
+                });
+                tx.responseDetails.add({
+                    messageId,
+                    chatId: chatId!,
+                    model,
+                    total_duration: intermediateResult?.total_duration,
+                    load_duration: intermediateResult?.load_duration,
+                    prompt_eval_count: intermediateResult?.prompt_eval_count,
+                    prompt_eval_duration: intermediateResult?.prompt_eval_duration,
+                    eval_count: intermediateResult?.eval_count,
+                    eval_duration: intermediateResult?.eval_duration
+                });
             });
-        });
+        } catch (e) {
+            console.log(e);
+            setError((e as Error).message);
+        }
+
         setInProgress(false);
         setPendingMessage(null);
     }, [selectedModel, messages, chatId]);
@@ -113,6 +122,7 @@ export default function Chat() {
                     messages?.map(msg => <MessageBox key={msg.id} message={msg}/>)
                 }
                 {pendingMessage && <MessageBox message={pendingMessage}/>}
+                {error && <p className={"text-muted-foreground"}>Something went wrong...</p>}
             </div>
             <div className={"p-2 md:w-1/2 max-md:w-4/5 place-self-center fixed bottom-0"}>
                 <ChatInput inProgress={inProgress} sendMessage={sendMessage} className={"place-self-center w-full"}/>
