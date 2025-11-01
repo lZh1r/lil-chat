01c452206c4d7721effce4a0b65d14f164c34e41
lZh1r <electroxhackerpig09@gmail.com>
2025-11-01 21:51:53 +0300

    style(chat): reduced the size of loading state circles

--- src/components/LoadingState.tsx
+++ src/components/LoadingState.tsx
@@ -1,9 +1,9 @@
 export default function LoadingState() {
     return (
         <div className={"flex space-x-5"}>
-            <div className={"rounded-full p-2 bg-foreground animate-bounce"}/>
-            <div className={"rounded-full p-2 bg-foreground animate-bounce delay-50"}/>
-            <div className={"rounded-full p-2 bg-foreground animate-bounce delay-100"}/>
+            <div className={"rounded-full p-1 bg-foreground animate-bounce"}/>
+            <div className={"rounded-full p-1 bg-foreground animate-bounce delay-50"}/>
+            <div className={"rounded-full p-1 bg-foreground animate-bounce delay-100"}/>
         </div>
     );
 }
