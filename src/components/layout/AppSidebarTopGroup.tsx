4a7b7a00eee97131e6c0b849bdc769d7c53c9ed2
lZh1r <electroxhackerpig09@gmail.com>
2025-11-02 23:11:27 +0300

    refactor: removed benchmark button

--- src/components/layout/AppSidebarTopGroup.tsx
+++ src/components/layout/AppSidebarTopGroup.tsx
@@ -1,7 +1,7 @@
 import {SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton} from "@/components/ui/sidebar.tsx";
 import {Tooltip, TooltipContent, TooltipTrigger} from "@/components/ui/tooltip.tsx";
 import {Link} from "react-router";
-import {Bot, MessageCirclePlus, Timer} from "lucide-react";
+import {Bot, MessageCirclePlus} from "lucide-react";
 
 export default function AppSidebarTopGroup(
     {
@@ -38,19 +38,6 @@ export default function AppSidebarTopGroup(
                         </TooltipTrigger>
                         <TooltipContent className={`${sidebarOpen && "hidden"}`} side={"right"}>
                             Models
-                        </TooltipContent>
-                    </Tooltip>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Link to={"/benchmarks"}>
-                                <SidebarMenuButton className={"cursor-pointer"}>
-                                    <Timer/>
-                                    <span>Benchmarks</span>
-                                </SidebarMenuButton>
-                            </Link>
-                        </TooltipTrigger>
-                        <TooltipContent className={`${sidebarOpen && "hidden"}`} side={"right"}>
-                            Benchmarks
                         </TooltipContent>
                     </Tooltip>
                 </SidebarMenu>
